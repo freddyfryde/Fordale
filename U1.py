0bc324832cf851d21b59f0f030b74a11b17709a5
Daniel Shigapov <80421548+Fordale@users.noreply.github.com>
2021-07-03 22:47:34 +0300

    Update U1.py

--- U1.py
+++ U1.py
@@ -1,51 +1,47 @@
 while True:
-  # Объявление переменных
   print ("####################")
   expression = input ("Введите пример: ")
   firstNum = ""
   secondNum = ""
   action = ""
   check = False
-  
-  # Возможность выхода из цикла
+
   if expression == "выход":
     break
-  
-  # Обработка выражения, запись значений в переменные
+
   for i in expression:
-    if check == False:
-      if i == "+":
-        check = True
-        action = "+"
-      elif i == "-":
-        check = True
-        action = "-"
-      elif i == "*":
-        check = True
-        action = "*"
-      elif i == "/":
-        check = True
-        action = "/"
-      elif i == " ":
-        None
+    try:  
+      i = int (i)
+      i = str (i)
+      if (check):
+        secondNum += i
       else:
-        if i == " ":
-          None
-        else:
-          firstNum += i
-    else:
-      secondNum += i
+        firstNum += i
+    except ValueError:
+      action += i
+      check = True
 
   firstNum = float (firstNum)
   secondNum = float (secondNum)
-  
-  # Проведение математических операций
+
   print ("Ответ:", end = " ")
-  if action == "+":
-    print (firstNum + secondNum)
-  if action == "-":
-    print (firstNum - secondNum)
-  if action == "*":
-    print (firstNum * secondNum)
-  if action == "/":
-    print (firstNum / secondNum)
+
+  try:
+    if action == "+":
+      print (firstNum + secondNum)
+    elif action == "-":
+      print (firstNum - secondNum)
+    elif action == "*":
+      print (firstNum * secondNum)
+    elif action == "/":
+      print (firstNum / secondNum)
+    elif action == "**":
+      print (firstNum ** secondNum)
+    elif action == "%":
+      print (firstNum % secondNum)
+    elif action == "//":
+      print (firstNum // secondNum)
+    else:
+      print ("не правильно выбрано действие")
+  except ZeroDivisionError:
+    print ("на 0 делить нельзя")
